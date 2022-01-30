9aec19070b73cb1fbb994772b9531466df6893f3
dbsqp <5895806+dbsqp@users.noreply.github.com>
2022-01-30 22:21:24 +0100

    Update get.sh

--- get.sh
+++ get.sh
@@ -10,17 +10,16 @@ fi
 while :
 do
   date
-  echo "--- Start Speedtest"
+  echo "### Start Speedtest ###"
   python3 speedtest.py
   RET=$?
   if [ ${RET} -ne 0 ];
   then
-    echo "Exit status not 0"
-    echo "Sleep 60 s"
+    echo "Exit status not 0, retry in 1 min"
     sleep 60
     python3 speedtest.py
   fi
   date
-  echo "Sleep $sleepPeriod s"
+  echo "### Sleep $sleepPeriod s"
   sleep $sleepPeriod
 done
